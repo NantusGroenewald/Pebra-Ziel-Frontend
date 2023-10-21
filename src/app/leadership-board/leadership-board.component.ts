ab8af61bfb27caf42a12f5ad6328111cf2443f12
Nantus <nantusg@agilebridge.co.za>
2023-10-21 15:35:57 +0200

    board updated

--- src/app/leadership-board/leadership-board.component.ts
+++ src/app/leadership-board/leadership-board.component.ts
@@ -8,16 +8,11 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079},
-  {position: 2, name: 'Helium', weight: 4.0026},
-  {position: 3, name: 'Lithium', weight: 6.941},
-  {position: 4, name: 'Beryllium', weight: 9.0122},
-  {position: 5, name: 'Boron', weight: 10.81},
-  {position: 6, name: 'Carbon', weight: 12.0107},
-  {position: 7, name: 'Nitrogen', weight: 14.0067},
-  {position: 8, name: 'Oxygen', weight: 15.9994},
-  {position: 9, name: 'Fluorine', weight: 18.9984},
-  {position: 10, name: 'Neon', weight: 20.1797}];
+  {position: 1, name: 'Nantus', weight: 413},
+  {position: 2, name: 'Hanroe', weight: 250},
+  {position: 3, name: 'Ian Joubert', weight: 178.65},
+  {position: 4, name: 'Casparus', weight: 14.2},
+  {position: 5, name: 'Tia', weight: 14.2},];
 @Component({
   selector: 'app-leadership-board',
   templateUrl: './leadership-board.component.html',
